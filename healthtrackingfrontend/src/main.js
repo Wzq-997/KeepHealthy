7b1ee3df28aff92334e98e9c62b0577dd19878e5
liuenli <2177816413@qq.com>
2024-06-11 20:21:30 +0800

    前端环境搭建+elementui

--- healthtrackingfrontend/src/main.js
+++ healthtrackingfrontend/src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// // import { createApp } from "vue";
+// // import App from "./App.vue";
+// // import router from "./router";
+// // import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import mitt from 'mitt'
+
+const app=createApp(App)
+app.config.globalProperties.emitter = mitt()
+
+app.use(router)
+app.mount('#app')
